b3fe0d84b5b7d1037b34d11be3c1b0cf4ea088ae
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-02-27 15:08:42 +0545

    add some functions like difference, product, and quotient

--- src/Sum_of_2_numbers.java
+++ src/Sum_of_2_numbers.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Sum_of_2_numbers {
     public static int sum (int a, int b){
         return a+b;
-
+    }
+    public static int difference (int a, int b){
+        return a - b;
+    }
+    public static int product (int a, int b){
+        return a * b;
+    }
+    public static int quotient (int a, int b){
+        return a / b;
     }
     static void main(String[] args) {
         Scanner nu = new Scanner(System.in);
@@ -14,8 +22,13 @@ public class Sum_of_2_numbers {
         System.out.print("Enter Second Integer number: ");
         number2 = nu.nextInt();
         nu.nextLine();
-        System.out.println("------- Sum -------");
-       String result = String.format("%d + %d = %d", number1, number2,sum(number1,number2));
+       String result = String.format("------- Sum ------- \n%d + %d = %d", number1, number2,sum(number1,number2));
         System.out.println(result);
+        String result1 = String.format("------- Difference ------- \n%d - %d = %d", number1, number2,difference(number1,number2));
+        System.out.println(result1);
+        String result2 = String.format("------- Product ------- \n%d * %d = %d", number1, number2,product(number1,number2));
+        System.out.println(result2);
+        String result3 = String.format("------- quotient ------- \n%d / %d = %d", number1, number2,quotient(number1,number2));
+        System.out.println(result3);
     }
 }
