59e550909b25bb1993c95b260236a1814f4b3fb5
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-03-27 15:11:14 +0545

    add new file, which is count the number of digits

---
+++ src/Count_digits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Count_digits {
+
+    public static int count_digit(int number){
+        int count = 0;
+
+        while (number > 0){
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter more then 1 digit number: ");
+        number = sc.nextInt();
+
+
+        System.out.println("The number of digits is "+count_digit(number));
+    }
+
+}
