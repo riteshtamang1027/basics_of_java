8fccead44b20c3115c673d7e8fc45757a371b126
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-04-16 22:12:47 +0545

    add print pattern file

---
+++ src/Pattern.java
@@ -0,0 +1,33 @@
+public class Pattern {
+   public static void main(String[] args) {
+
+       for (int i=1;i<=5;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print("* \t");
+           }
+           System.out.println(" ");
+       }
+
+       System.out.println("----------------\n");
+
+    for (int i=1; i<=5;i++){
+        for (int j = 5; j>=i;j--){
+            System.out.print("* \t");
+        }
+        System.out.println(" ");
+    }
+
+       System.out.println("----------------\n");
+
+       for (int i = 1; i<=5; i++){
+           for(int j = 1; j<=i;j++){
+               System.out.print(j+"\t");
+           }
+           System.out.println(" ");
+
+       }
+    }
+
+
+
+}
