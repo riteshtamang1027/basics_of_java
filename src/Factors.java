fdb33730c057fe3ff5be0b5c7296d40a6e48213d
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-04-18 21:15:12 +0545

    add new function in factors file

--- src/Factors.java
+++ src/Factors.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Factors {
 
+//    possible factors
     public static void factors(int number){
         int i;
         System.out.printf("The factors of the Number %d is \n",number);
@@ -11,6 +12,17 @@ public class Factors {
             }
         }
     }
+
+//    count the factors
+    public static void count_factor(int number){
+        int count = 0,i;
+        for(i=1;i<=number;i++){
+            if (number %i ==0)
+                count +=1;
+        }
+        System.out.printf("The number of possible factors of %d is %d \n",number,count);
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        int number;
@@ -18,6 +30,7 @@ public class Factors {
         number = sc.nextInt();
 
         factors(number);
+        count_factor(number);
 
     }
 }
