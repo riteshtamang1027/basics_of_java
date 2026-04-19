384918d5f63cce07f6567f491fa376b6fb3e5022
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-04-19 08:40:13 +0545

    use function to check the number is a strong or not

--- src/Strong_number.java
+++ src/Strong_number.java
@@ -2,24 +2,47 @@ import java.util.Scanner;
 
 public class Strong_number {
 
+//    function which check the number is a strong or not.
+    public static void check_strong(int number){
+        int i, reminder, sum =  0, original = number;
+        while(number > 0){
+            reminder = number % 10;
+
+            int factorial = 1;
+            for(i = 1; i <= reminder; i++){
+                factorial = factorial * i;
+            }
+
+            sum += factorial;
+
+            number /= 10;
+        }
+
+        System.out.println("The total sum of enter number factorial is: "+sum);
+// if the sum of the input number factorial is equal to the enter number then it show the enter number is a strong number
+        if (original == sum){
+            System.out.println("The input number is strong number.");
+        }
+        else {
+            System.out.println("The input number isn't strong number.");
+        }
+
+    }
+
 
    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int number = 124
-                ,reminder, fact_sum=0;
+        int number;
+       System.out.print("Enter number: ");
 
-        while (number >0){
-            reminder = number %10;
-           int fact=1;
-            for (int i=1;i<=reminder;i++){
-                fact= fact *i;
-            }
-            fact_sum+=fact;
-            number /=10;
-        }
-       System.out.println(fact_sum);
+       number = sc.nextInt();
+
+//       function call
+       check_strong(number);
+
+
 
 
     }
