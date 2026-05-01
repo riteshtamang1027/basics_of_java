6cb24ebe142ac650758800d52e0d872d4181a74d
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-05-01 21:17:23 +0545

    add selective sort function

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,4 +1,58 @@
 public class Sorting {
+
+
+//    bubble sort
+    public static void bubbleSort(int array[]){
+        int length = array.length,i;
+
+        System.out.println("\nBobble sorting array elements:");
+
+        for( i=0; i<length-1; i++){
+            for(int j=0; j< length-i-1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j]= array[j+1];
+                    array[j+1]= temp;
+                }
+            }
+        }
+
+
+        for ( i=0; i<length;i++){
+            System.out.printf("%d,",array[i]);
+        }
+    }
+
+
+//    Selection sort
+
+    public static void selectionSort(int arr[]){
+
+        int i,j, length=arr.length;
+
+        System.out.println("\nSelective sorting array element");
+
+        for(i=0; i< length; i++){
+            int smallest = i;
+
+            for (j=i+1; j < length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+
+                }
+            }
+
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+
+        }
+for (i=0; i<length;i++) {
+    System.out.printf("%d,", arr[i]);
+}
+    }
+
+
     public static void main(String[] args) {
 
         int []array = {10,5,11,9,8,15};
@@ -8,24 +62,14 @@ public class Sorting {
         System.out.println("Before sorting array elements: ");
         for (int i=0; i< length;i++){
 
-            System.out.println(array[i]);
+            System.out.printf("%d,",array[i]);
 
         }
 
-        System.out.println("After sorting array elements:");
 
-        for(int i=0; i<length-1; i++){
-            for(int j=0; j< length-i-1; j++){
-               if(array[j] > array[j+1]){
-                   int temp = array[j];
-                   array[j]= array[j+1];
-                   array[j+1]= temp;
-               }
-            }
-        }
+//        bubbleSort(array);
 
-        for (int i=0; i<length;i++){
-            System.out.println(array[i]);
-        }
+        selectionSort(array);
+
     }
 }
