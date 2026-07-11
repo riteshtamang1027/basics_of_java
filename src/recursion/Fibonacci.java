78a59e53561c96f236e2dbe4e34898c2da03bb1c
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-07-11 17:44:46 +0545

    add new fibonacci file

--- src/recursion/Fibonacci.java
+++ src/recursion/Fibonacci.java
@@ -15,13 +15,45 @@ if(i == 0){
 
     }
 
+
+//    using for lop to print the power of a number;
+    static void power(int bas, int pow){
+        System.out.println("Calculate the power of a number using for loop:");
+        int res=1;
+        for(int i = 0; i< pow; i++){
+            res *= bas;
+        }
+        System.out.println(res);
+    }
+
+//    print the power of a number using recursion;
+    public static int printPowerOfNumber(int a, int n){
+
+        if(n == 0){
+
+            return 1;
+        } else if (a == 0) {
+
+            return 0;
+        }
+
+        int pow1 = printPowerOfNumber(a, n-1);
+       int res = a * pow1;
+       return  res;
+
+
+    }
+
     static void main(String[] args) {
 
         int a=0, b=1, n=10;
         System.out.println(a);
         System.out.println(b);
 
-        printFibonacci(a,b,n-2);
+//        printFibonacci(a,b,n-2);l
+        System.out.println("Calculate the power of a number using recursion:");
+        System.out.println(printPowerOfNumber(2,5));
+        power(2,5);
 
     }
 }
